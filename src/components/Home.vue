bc36342c03217e7da1d84edc296399847340e056
hero <hyw163@163.com>
2019-04-23 22:11:02 +0800

    初始化 完成

---
+++ src/components/Home.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <el-container>
+      <el-header>
+        <el-button type="info" @click="logout">退出</el-button>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      this.$confirm('确定退出吗', '退出', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$router.push('/login')
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.el-container {
+  height: 100%;
+  .el-aside {
+    background-color: #333744;
+  }
+}
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+}
+
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+}
+
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,93 @@
+<template>
+  <div id="login-container">
+    <!-- <img src="../assets/img/A.svg" alt=""> -->
+    <div id="login-box">
+      <div id="login-logo">
+        <img src="../assets/img/c.jpg" alt>
+      </div>
+      <el-form :model="loginForm" ref="refform" label-width="100px">
+        <el-form-item label="用户名" prop="username">
+          <el-input type="text" v-model="loginForm.username" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="password">
+          <el-input type="password" v-model="loginForm.password" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-col :push="12">
+            <el-button type="primary" @click="login">登录</el-button>
+            <el-button type="info" @click="reset">重置</el-button>
+          </el-col>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    login() {
+      this.$router.push('/home')
+    },
+    reset() {
+      this.$refs.refform.resetFields()
+    }
+  },
+  data() {
+    return {
+      loginForm: {
+        username: 'admin',
+        password: '1111'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+#login-container {
+  height: 100%;
+  width: 100%;
+  background: url('../assets/img/q.jpg') no-repeat;
+  background-size: 100%, 100%;
+  overflow: hidden;
+  #login-box {
+    width: 450px;
+    height: 350px;
+    border-radius: 5px;
+    box-shadow: 0, 0, 5px;
+    background-color: #0b548a;
+    position: absolute;
+    left: 75%;
+    top: 40%;
+    transform: translate(-50%, -50%);
+    opacity: 0.8;
+    #login-logo {
+      border-radius: 51%;
+      width: 150px;
+      height: 150px;
+      position: absolute;
+      left: 50%;
+
+      transform: translate(-50%, -50%);
+      background: #ccc;
+      img {
+        border-radius: 51%;
+        width: 100%;
+        height: 100%;
+      }
+    }
+    .el-form {
+      position: absolute;
+      bottom: 0;
+      box-sizing: border-box;
+      width: 100%;
+      padding: 0 20px 0 0;
+      color: #fff;
+      .el-form-item {
+        color: #fff;
+      }
+    }
+  }
+}
+</style>
